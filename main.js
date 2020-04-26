866b9f2fdd76deee71b270bf6ff78ac36510adef
David Aerne <meodai@gmail.com>
2020-04-26 19:05:40 +0200

    feat(docs): links to google docs

--- main.js
+++ main.js
@@ -20,14 +20,14 @@ const parseEntries = entries => {
 };
 
 const $container = document.querySelector('[data-entries]');
-const gSheetJSONsrc = 'https://spreadsheets.google.com/feeds/cells/1O36xe8y9oOeYBOr0mvS7NZLDm83hilO0WB9f75CRnJA/1/public/full?alt=json&rnd=' + 1000 * Math.random() 
+const gSheetJSONsrc = 'https://spreadsheets.google.com/feeds/cells/1O36xe8y9oOeYBOr0mvS7NZLDm83hilO0WB9f75CRnJA/1/public/full?alt=json&rnd=' + 1000 * Math.random()
 
 fetch(gSheetJSONsrc).then((resp) => (resp.json())).then((body) => {
   const entries = [...body.feed.entry];
-  const entriesArr = parseEntries(entries);
+  const entriesArr = [...parseEntries(entries)];
   entriesArr.shift();
   const entriesHTML = entriesArr.reduce((r, d) => (
-    r + `<details><summary><strong>[FDI]</strong> ${d[0]}</summary><p>${d[1]}</p></details>`
+    r + `<details><summary><strong>[FDI]</strong> ${d[1]}</summary><p>${d[2]}</p></details>`
   ),'')
 
   $container.innerHTML = entriesHTML;
